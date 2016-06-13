9973a2e81e6ac71b68ae5bab0de070ac2caede98
David Klaftenegger <david.klaftenegger@it.uu.se>
2016-06-13 21:07:58 +0200

    Remove code duplication from backend
    The data distribution code is currently replicated in the backend.
    This patch switches the behaviour to rely on the data_distribution
    header files instead, providing a compatibility layer based on
    global_ptr<char>.

--- src/backend/mpi/swdsm.cpp
+++ src/backend/mpi/swdsm.cpp
@@ -4,6 +4,7 @@
  * @copyright Eta Scale AB. Licensed under the Eta Scale Open Source License. See the LICENSE file for details.
  */
 #include "virtual_memory/virtual_memory.hpp"
+#include "data_distribution/data_distribution.hpp"
 #include "swdsm.h"
 
 namespace vm = argo::virtual_memory;
@@ -598,22 +599,16 @@ void handler(int sig, siginfo_t *si, void *unused){
 	return;
 }
 
-
 unsigned long getHomenode(unsigned long addr){
-	unsigned long homenode = addr/size_of_chunk;
-	if(homenode >=(unsigned long)numtasks){
-		exit(EXIT_FAILURE);
-	}
-	return homenode;
+	using namespace argo::data_distribution;
+	global_ptr<char> gptr(reinterpret_cast<char*>(addr + reinterpret_cast<unsigned long>(startAddr)));
+	return gptr.node();
 }
 
 unsigned long getOffset(unsigned long addr){
-	//offset in local memory on remote node (homenode)
-	unsigned long offset = addr - (getHomenode(addr))*size_of_chunk;
-	if(offset >=size_of_chunk){
-		exit(EXIT_FAILURE);
-	}
-	return offset;
+	using namespace argo::data_distribution;
+	global_ptr<char> gptr(reinterpret_cast<char*>(addr + reinterpret_cast<unsigned long>(startAddr)));
+	return gptr.offset();
 }
 
 void *writeloop(void * x){
